5e9e454724e8bda9de68c22618f5bebc315cf3fa
Leesumok <leesumok_@naver.com>
2025-07-10 14:33:07 +0900

    fix: swagger config 추가

---
+++ src/main/kotlin/com/goorm/maki/config/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package com.goorm.maki.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Nollea API 문서")
+                    .description("API 명세서입니다")
+                    .version("v1.0.0")
+            )
+    }
+}
