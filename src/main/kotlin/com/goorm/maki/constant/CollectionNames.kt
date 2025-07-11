1cb76247581ddfa41ecb438732773caf982be426
Leesumok <leesumok_@naver.com>
2025-07-11 09:40:53 +0900

    fix: db collection 변경

--- src/main/kotlin/com/goorm/maki/constant/CollectionNames.kt
+++ src/main/kotlin/com/goorm/maki/constant/CollectionNames.kt
@@ -1,6 +1,6 @@
 package com.goorm.maki.constant
 
 object CollectionNames {
-    const val NIGHT_TOUR = "night_tour"
-    const val NIGHT_TOUR_IMAGE = "night_tour_image"
+    const val NIGHT_TOUR = "night_tour_v2"
+    const val NIGHT_TOUR_IMAGE = "night_tour_image_v2"
 }
